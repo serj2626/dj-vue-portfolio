8af0fb86fb39eb6c06754cd2e8081991fe850eda
serj2626 <serj2626@mail.ru>
2024-06-24 02:03:38 +0300

    create backend

---
+++ backend/backend/settings.py
@@ -0,0 +1,305 @@
+import os
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+
+SECRET_KEY = 'django-insecure-=%5=#0@55#dept-t!2*nboia7z4t+#ibg3)uown51#0vy#cg@*'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5173",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://127.0.0.1:5173",
+]
+
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # local apps
+    'contacts',
+    'portfolio',
+
+    # 3rd party
+    'rest_framework',
+    'corsheaders',
+    'django_extensions',
+    'django_ckeditor_5',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'backend.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'backend.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.0/topics/i18n/
+
+LANGUAGE_CODE = 'ru'
+
+TIME_ZONE = 'Europe/Moscow'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# CKEDITOR-5
+################################################################
+customColorPalette = [
+    {"color": "hsl(4, 90%, 58%)", "label": "Red"},
+    {"color": "hsl(340, 82%, 52%)", "label": "Pink"},
+    {"color": "hsl(291, 64%, 42%)", "label": "Purple"},
+    {"color": "hsl(262, 52%, 47%)", "label": "Deep Purple"},
+    {"color": "hsl(231, 48%, 48%)", "label": "Indigo"},
+    {"color": "hsl(207, 90%, 54%)", "label": "Blue"},
+]
+
+
+
+CKEDITOR_5_CONFIGS = {
+    "default": {
+        "toolbar": [
+            "heading",
+            "|",
+            "bold",
+            "italic",
+            "link",
+            "bulletedList",
+            "numberedList",
+            "blockQuote",
+            "imageUpload"
+        ],
+    },
+    "comment": {
+        "language": {"ui": "ru", "content": "ru"},
+        "toolbar": [
+            "heading",
+            "|",
+            "bold",
+            "italic",
+            "link",
+            "bulletedList",
+            "numberedList",
+            "blockQuote",
+        ],
+    },
+    "extends": {
+        "language": "ru",
+        "blockToolbar": [
+            "paragraph",
+            "heading1",
+            "heading2",
+            "heading3",
+            "|",
+            "bulletedList",
+            "numberedList",
+            "|",
+            "blockQuote",
+        ],
+        "toolbar": [
+            "heading",
+            "codeBlock",
+            "|",
+            "outdent",
+            "indent",
+            "|",
+            "bold",
+            "italic",
+            "link",
+            "underline",
+            "strikethrough",
+            "code",
+            "subscript",
+            "superscript",
+            "highlight",
+            "|",
+            "bulletedList",
+            "numberedList",
+            "todoList",
+            "|",
+            "blockQuote",
+            "insertImage",
+            "|",
+            "fontSize",
+            "fontFamily",
+            "fontColor",
+            "fontBackgroundColor",
+            "mediaEmbed",
+            "removeFormat",
+            "insertTable",
+            "sourceEditing",
+        ],
+        "image": {
+            "toolbar": [
+                "imageTextAlternative",
+                "|",
+                "imageStyle:alignLeft",
+                "imageStyle:alignRight",
+                "imageStyle:alignCenter",
+                "imageStyle:side",
+                "|",
+                "toggleImageCaption",
+                "|"
+            ],
+            "styles": [
+                "full",
+                "side",
+                "alignLeft",
+                "alignRight",
+                "alignCenter",
+            ],
+        },
+        "table": {
+            "contentToolbar": [
+                "tableColumn",
+                "tableRow",
+                "mergeTableCells",
+                "tableProperties",
+                "tableCellProperties",
+            ],
+            "tableProperties": {
+                "borderColors": customColorPalette,
+                "backgroundColors": customColorPalette,
+            },
+            "tableCellProperties": {
+                "borderColors": customColorPalette,
+                "backgroundColors": customColorPalette,
+            },
+        },
+        "heading": {
+            "options": [
+                {
+                    "model": "paragraph",
+                    "title": "Paragraph",
+                    "class": "ck-heading_paragraph",
+                },
+                {
+                    "model": "heading1",
+                    "view": "h1",
+                    "title": "Heading 1",
+                    "class": "ck-heading_heading1",
+                },
+                {
+                    "model": "heading2",
+                    "view": "h2",
+                    "title": "Heading 2",
+                    "class": "ck-heading_heading2",
+                },
+                {
+                    "model": "heading3",
+                    "view": "h3",
+                    "title": "Heading 3",
+                    "class": "ck-heading_heading3",
+                },
+            ]
+        },
+        "list": {
+            "properties": {
+                "styles": True,
+                "startIndex": True,
+                "reversed": True,
+            }
+        },
+        "htmlSupport": {
+            "allow": [
+                {"name": "/.*/", "attributes": True, "classes": True, "styles": True}
+            ]
+        },
+    },
+}
+# END CKEDITOR-5
+###########################################################################################
---
+++ backend/portfolio/models.py
@@ -0,0 +1,121 @@
+from django.db import models
+from django_ckeditor_5.fields import CKEditor5Field
+from pytils.translit import slugify
+
+
+class GenericModel(models.Model):
+    slug = models.SlugField(max_length=100, null=True,
+                            unique=True, verbose_name='URL')
+
+    def save(self, *args, **kwargs):
+        self.slug = self.slug or slugify(self.title)
+        super().save(*args, **kwargs)
+
+    class Meta:
+        abstract = True
+
+
+class Course(GenericModel):
+    title = models.CharField(max_length=100, verbose_name='Название курса')
+    company = models.CharField(max_length=100, verbose_name='Учебное заведение')
+    description = CKEditor5Field(
+        verbose_name="Описание курса", config_name="extends")
+    date_start = models.DateField(
+        auto_now_add=True, verbose_name='Дата начала')
+    date_end = models.DateField(auto_now=True,
+                                verbose_name='Дата окончания', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+    def __str__(self):
+        return f'Курс: {self.title}'
+
+class Experience(GenericModel):
+    vacancy_title = models.CharField(
+        max_length=100, verbose_name='Название вакансии')
+    company = models.CharField(max_length=100, verbose_name='Компания')
+    responsibilities = CKEditor5Field(
+        verbose_name="Обязанности", config_name="extends")
+    date_start = models.DateField(
+        auto_now_add=True, verbose_name='Дата начала')
+    date_end = models.DateField(auto_now=True,
+                                verbose_name='Дата окончания', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Опыт работы'
+        verbose_name_plural = 'Опыт работы'
+
+    def __str__(self):
+        return f'Опыт работы: {self.vacancy_title}'
+
+class Skill(GenericModel):
+    title = models.CharField(max_length=100, verbose_name='Название')
+
+    class Meta:
+        verbose_name = 'Навык'
+        verbose_name_plural = 'Навыки'
+
+    def __str__(self):
+        return self.title
+
+
+class Stack(GenericModel):
+    title = models.CharField(max_length=100, verbose_name='Название стека')
+    
+    class Meta:
+        verbose_name = 'Стек'
+        verbose_name_plural = 'Стеки'
+
+    def __str__(self):
+        return self.title
+
+
+class Project(GenericModel):
+    title = models.CharField(max_length=100, verbose_name='Название проекта')
+    description = CKEditor5Field(
+        verbose_name="Описание проекта", config_name="extends")
+    stack = models.ManyToManyField(Stack, verbose_name='Стеки проекта')
+    avatar = models.ImageField(
+        upload_to='portfolio/projects', null=True, verbose_name='Аватар проекта')
+    url = models.URLField(verbose_name='Ссылка на проект',
+                          null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Проект'
+        verbose_name_plural = 'Проекты'
+
+    def __str__(self):
+        return f'Проект {self.title}'
+
+
+class Resume(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Имя')
+    surname = models.CharField(max_length=100, verbose_name='Фамилия')
+    position = models.CharField(max_length=100, verbose_name='Должность')
+    stack = models.ManyToManyField(
+        Stack, verbose_name='Стек', blank=True)
+    skill = models.ManyToManyField(
+        Skill, verbose_name='Навыки', blank=True)
+    course = models.ManyToManyField(
+        Course, verbose_name='Курсы', blank=True)
+    experience = models.ManyToManyField(
+        Experience, verbose_name='Опыт работы', blank=True)
+    min_salary = models.IntegerField(
+        verbose_name='Минимальная зарплата', blank=True, null=True)
+    max_salary = models.IntegerField(
+        verbose_name='Максимальная зарплата,', blank=True, null=True)
+    about = CKEditor5Field(
+        verbose_name="О себе", config_name="extends")
+    github_url = models.URLField(
+        verbose_name='Ссылка на GitHub', null=True, blank=True)
+    avatar = models.ImageField(
+        upload_to='portfolio/resumes', null=True, verbose_name='Аватар резюме')
+
+    class Meta:
+        verbose_name = 'Резюме'
+        verbose_name_plural = 'Резюме'
+
+    def __str__(self):
+        return f'Резюме {self.position}'
