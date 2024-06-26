b91ad1e80977ae2cf025bfc39234025c70db266c
serj2626 <serj2626@mail.ru>
2024-06-26 03:14:36 +0300

    new model  and serializer contact

--- backend/contacts/admin.py
+++ backend/contacts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    '''Admin View for Contact)'''
+
+    list_display = ('name', 'email', 'message', 'created_at')
