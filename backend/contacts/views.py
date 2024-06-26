425fd56fa6243da639a90d4fb3e792104423b0b8
serj2626 <serj2626@mail.ru>
2024-06-26 03:15:57 +0300

    new view for add new contacts

--- backend/contacts/views.py
+++ backend/contacts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Contact
+from .serializers import ContactSerializer
 
-# Create your views here.
+
+class ContactList(generics.CreateAPIView):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
