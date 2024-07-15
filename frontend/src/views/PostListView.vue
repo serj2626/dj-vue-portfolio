4fae60db330792cc0cf9c16689743d6fe8bff0e6
serj2626 <serj2626@mail.ru>
2024-07-15 03:36:58 +0300

    new lines in comp PostList

--- frontend/src/views/PostListView.vue
+++ frontend/src/views/PostListView.vue
@@ -3,13 +3,6 @@ import axios from "axios";
 import { onMounted, ref } from "vue";
 import { useRouter } from "vue-router";
 
-import { Swiper, SwiperSlide } from "swiper/vue";
-import "swiper/css";
-import { EffectCoverflow, Pagination } from "swiper/modules";
-import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
-
-const modules = [EffectCoverflow, Pagination];
 
 const router = useRouter();
 const projects = ref([]);
@@ -17,6 +10,7 @@ const projects = ref([]);
 const getProjects = async () => {
   const response = await axios.get("/api/projects/");
   projects.value = response.data;
+  console.log(projects.value);
 };
 
 onMounted(() => {
@@ -25,67 +19,24 @@ onMounted(() => {
 </script>
 
 <template>
-  <swiper
-    :effect="'coverflow'"
-    :grabCursor="true"
-    :centeredSlides="true"
-    :slidesPerView="'auto'"
-    :coverflowEffect="{
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true,
-    }"
-    :pagination="false"
-    :modules="modules"
-    class="mySwiper"
-  >
-    <swiper-slide v-for="project in projects" :key="project.id">
-      <div
-        @click="
-          router.push({ name: 'project', params: { slug: project.slug } })
-        "
-        class="card"
-      >
-        <img
-          :src="project.avatar"
-          class="card-img-top"
-          :alt="project.title"
-          :title="project.title"
-        />
-        <div class="card-body">
-          <h5 class="card-title text-center">{{ project.title }}</h5>
+
+  <div class="row my-5">
+
+    <div class="w-75 mx-auto">
+
+
+      <div v-for="project in projects" :key="project.id" 
+      class="d-flex align-items-center justify-content-between mb-5">
+        <h4 class="title">{{ project.title }}</h4>
+        <div class="list-stack" v-for="stack in project.stack" :key="stack.id">
+          <img width="35" height="auto" class="logo-stack" :src="stack.img" :alt="stack.title" :title="stack.title" />
         </div>
       </div>
-    </swiper-slide>
-  </swiper>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-.swiper {
-  width: 100%;
-  padding-block: 100px;
-}
-
-.swiper-slide {
-  background-position: center;
-  background-size: cover;
-  width: 600px;
-  height: 500px;
-}
-
-.card {
-  width: 600px;
-  height: 500px;
-}
-
-.swiper-slide img {
-  display: block;
-  height: 100%;
-  width: 100%;
-}
-
 .title {
   color: #03e9f4;
   text-shadow: 0 0 2px wheat;
