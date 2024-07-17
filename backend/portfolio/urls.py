7bc11e14562e6a77b7800991aeee669ad16b390e
serj2626 <serj2626@mail.ru>
2024-07-17 03:54:34 +0300

    create view for list and detail post

--- backend/portfolio/urls.py
+++ backend/portfolio/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import ProjectListView, StackListView, ResumeView, ProjectDetailView
-
+from posts.views import PostListView, PostDetailView
 
 urlpatterns = [
     path("stacks/", StackListView.as_view(), name="stack-list"),
     path("resume/", ResumeView.as_view(), name="resume"),
     path("projects/", ProjectListView.as_view(), name="project-list"),
     path("projects/<slug:slug>/", ProjectDetailView.as_view(), name="project-detail"),
+    path("posts/", PostListView.as_view(), name="post-list"),
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
 ]
--- backend/posts/views.py
+++ backend/posts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from .models import Article
+from .serializers import ArticleSerializer
+from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema
 
-# Create your views here.
+
+class PostListView(ListAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+
+class PostDetailView(RetrieveAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
